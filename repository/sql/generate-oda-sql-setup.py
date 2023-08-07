d488dc098901e091d587cbe07d185d22072b8756
Henrik Korsgaard <korsgaard@cs.au.dk>
2023-08-07 15:51:06 +0200

    more issues with MSSQL. Fix in notes

--- repository/sql/generate-oda-sql-setup.py
+++ repository/sql/generate-oda-sql-setup.py
@@ -1,1 +1,23 @@
-I should totally use python to generate the psql and slq lite files for creating a database. We want both.
+# I should totally use python to generate the psql and slq lite files for creating a database. We want both.
+
+# https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
+
+
+import pyodbc #pip install pyodbc  
+ 
+server = 'localhost' 
+database = 'ODA' 
+username = 'sa' 
+password = '' ## need a test user with a different password 
+# ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+cursor.execute("SELECT @@version;") 
+row = cursor.fetchone() 
+while row: 
+    print(row[0])
+    row = cursor.fetchone()
+
+
+# How to handle error https://stackoverflow.com/questions/57265913/error-tcp-provider-error-code-0x2746-during-the-sql-setup-in-linux-through-te/57453901#57453901
