43c520b0042f7c998a846adbb5e34b23355068e9
Henrik Korsgaard <korsgaard@cs.au.dk>
2023-08-11 14:59:52 +0200

    added Sag to the schema. This will fail the schema test so that I remember what to implement next

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ import (
 func main() {
 
 	godotenv.Load("config_dev.env")
-	os.Setenv("SQLITE_DATABASE_PATH", "ingest/data/odatest.sqlite.db")
+	os.Setenv("SQLITE_DATABASE_PATH", "ingest/data/oda.sqlite.db")
 
 	b, err := ioutil.ReadFile("./schema/schema.graphql")
 	if err != nil {
@@ -31,7 +31,7 @@ func main() {
 
 	qr := resolvers.QueryResolver{}
 
-    schema := graphql.MustParseSchema(string(b), &qr, graphql.MaxDepth(3))
+    schema := graphql.MustParseSchema(string(b), &qr, graphql.MaxDepth(5))
 	http.HandleFunc("/", handleTestClient)
 	http.Handle("/graphiql", graphiqlHandler)
     http.Handle("/graphql", &relay.Handler{Schema: schema})
