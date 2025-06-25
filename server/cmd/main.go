1fdde6f6f4fc4f0c28cf4357ffac96e46fb41962
Henrik Korsgaard <korsgaard@protonmail.com>
2025-06-25 22:23:45 +0200

    feat: add .env support

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/henrikkorsgaard/folketingets-aabne-data/ftoda"
 	"github.com/henrikkorsgaard/folketingets-aabne-data/handlers"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	dbFile := os.Getenv("DB_FILE")
+	odataHost := os.Getenv("ODATA_HOST")
+	fmt.Println(dbFile)
+	fmt.Println(odataHost)
 	ftodaService := ftoda.NewFTODAService()
 	fmt.Println("Server is running on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", handlers.NewServer(&ftodaService)))
