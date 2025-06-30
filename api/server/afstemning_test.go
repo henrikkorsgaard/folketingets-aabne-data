aa24f361c7c996632dc2c536f95a64eafd9bc43c
Henrik Korsgaard <korsgaard@protonmail.com>
2025-06-30 20:15:46 +0200

    feat: created test stub for getting a vote for a particular legislative vote

---
+++ api/server/afstemning_test.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func TestAfstemningFromSagId(t *testing.T) {
+	is := is.New(t)
+
+	is.Equal(true, false) //expect length to be 100
+}
